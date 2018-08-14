e26a795d73e376192bde1d8a6317375b4e47983e
Rei Colina <reinaldo13@gmail.com>
2018-08-14 13:19:12 -0700

    Fix modules.

--- constants/config.js
+++ constants/config.js
@@ -7,9 +7,9 @@
  */
 
 module.exports = {
-    FB_SHARE_URL = 'https://www.facebook.com/sharer/sharer.php?u=',
-    TW_SHARE_URL = 'https://twitter.com/intent/tweet?text=',
-    LI_SHARE_URL = 'https://www.linkedin.com/shareArticle?mini=true&url=',
-    FB_API_URL = 'https://graph.facebook.com/v2.6/me/'
+    FB_SHARE_URL: 'https://www.facebook.com/sharer/sharer.php?u=',
+    TW_SHARE_URL: 'https://twitter.com/intent/tweet?text=',
+    LI_SHARE_URL: 'https://www.linkedin.com/shareArticle?mini=true&url=',
+    FB_API_URL: 'https://graph.facebook.com/v2.6/me/'
 }
 
--- constants/events.js
+++ constants/events.js
@@ -7,10 +7,10 @@
  */
 
 module.exports = {
-    TOPIC_TECH = 'topic-tech',
-    GET_STARTED = 'get-started',
-    MARK_SEEN = 'mark_seen',
-    TYPING_ON = 'typing_on',
-    TYPING_OFF = 'typing_off'
+    TOPIC_TECH: 'topic-tech',
+    GET_STARTED: 'get-started',
+    MARK_SEEN: 'mark_seen',
+    TYPING_ON: 'typing_on',
+    TYPING_OFF: 'typing_off'
 }
 
