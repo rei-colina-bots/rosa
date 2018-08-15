05d74db3535b5a1f6f6e12f15586da9404866a61
Rei Colina <reinaldo13@gmail.com>
2018-08-15 06:16:35 -0700

    Replacing Feeds.

--- constants/text.js
+++ constants/text.js
@@ -18,7 +18,7 @@ module.exports = {
     MENU_BBC: '🌎 BBC',
     MENU_REUTERS: '🌎 Reuters',
     MENU_HBR: '💼 Harvard Business Review',
-    MENU_INC_LEAD: '💼 Inc.com - Leadership',
+    MENU_INC_LEAD: '💼 Inc - Leadership',
     MENU_WIRED: '🔌 Wired',
     MENU_SOCIAL: '📱 Social Networks',
     TWITTER: 'Twitter',
