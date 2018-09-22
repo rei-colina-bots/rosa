5d82dcd9b73fd65e8b95f0464e1e588449842e85
Rei Colina <reinaldo13@gmail.com>
2018-09-22 12:18:37 -0700

    Integrate to Amplify.

--- helpers/oauth.js
+++ helpers/oauth.js
@@ -23,9 +23,15 @@ const postRequest = (baseUrl, payload) => {
     return new Promise(function (resolve, reject) {
         console.log(baseUrl + 'oauth2/token');
         console.log(payload);
+        let auth = "Basic " + new Buffer(
+            process.env.HOOTSUITE_CLIENT_ID
+            + ":" + process.env.HOOTSUITE_CLIENT_SECRET).toString("base64");
         request({
             uri: baseUrl + 'oauth2/token',
             method: 'POST',
+            headers : {
+                "Authorization" : auth
+            },
             json: payload
         }, (err, res, body) => {
             if (err) {
